9bf99e5a37ca5c1d6f543645de4f3c1eb3109e56
Samuel Zheng <z5418112@ad.unsw.edu.com>
2022-12-11 22:42:27 +1300

    fix linting

--- src/server.ts
+++ src/server.ts
@@ -1,25 +1,24 @@
 import express from 'express';
 import cors from 'cors';
 import morgan from 'morgan';
-import * as dotenv from 'dotenv'
+import * as dotenv from 'dotenv';
 
 // Init
 const CONFIG = dotenv.config().parsed;
-let app = express();
+const app = express();
 app.use(express.json());
 app.use(cors());
 app.use(morgan((process.env.NODE_ENV !== 'production') ? 'dev' : 'common'));
 
 // Routes
 app.get(CONFIG.BASE_URL, (req, res) => {
-    return res.json({ message: "Welcome to the MOCBOT API. Visit https://github.com/MasterOfCubesAU/mocbot-api" })
+  return res.json({ message: 'Welcome to the MOCBOT API. Visit https://github.com/MasterOfCubesAU/mocbot-api' });
 });
 
-
 const server = app.listen(parseInt(CONFIG.PORT), CONFIG.HOST, () => {
-    console.log(`⚡️ Server listening on port ${CONFIG.PORT} at ${CONFIG.HOST}`);
+  console.log(`⚡️ Server listening on port ${CONFIG.PORT} at ${CONFIG.HOST}`);
 });
 
 process.on('SIGINT', () => {
-    server.close(() => console.log('Shutting down server gracefully.'));
+  server.close(() => console.log('Shutting down server gracefully.'));
 });
