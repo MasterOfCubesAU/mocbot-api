8eb22f0b7f439bf3adee4beb200590fdd291a78c
MasterOfCubesAU <masterofcubesau@gmail.com>
2022-12-13 15:08:59 +1100

    Added /xp route

---
+++ tests/v1/xp/xp.test.ts
@@ -0,0 +1,22 @@
+import { http } from "../utils"
+
+const ROUTE = "/v1/xp"
+
+describe("Guild XP Data", () => {
+    describe(`GET`, () => {
+        test('Valid', () => {
+            const request = http('GET', `${ROUTE}/231230403053092864`, undefined);
+            const response = JSON.parse(String(request.getBody() as string));
+            expect(response).toStrictEqual({});
+        });
+    });
+});
+describe("User XP Data", () => {
+    describe(`GET`, () => {
+        test('Valid', () => {
+            const request = http('GET', `${ROUTE}/231230403053092864/169402073404669952`, undefined);
+            const response = JSON.parse(String(request.getBody() as string));
+            expect(response).toStrictEqual({});
+        });
+    });
+});
