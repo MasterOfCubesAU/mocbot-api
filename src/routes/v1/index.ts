7a74e8440561534fb235f08a058d7f5dbb99bace
MasterOfCubesAU <masterofcubesau@gmail.com>
2022-12-13 15:10:51 +1100

    Updated /v1 routes

--- src/routes/v1/index.ts
+++ src/routes/v1/index.ts
@@ -2,9 +2,13 @@ import express from 'express'
 
 // Import API routes
 import settingsRouter from "./settings";
+import xpRouter from "./xp";
+import warningsRouter from "./warnings";
 
-const router = express.Router()
+const router = express.Router();
 
-router.use("/settings", settingsRouter)
+router.use("/settings", settingsRouter);
+router.use("/xp", xpRouter);
+router.use("/warnings", warningsRouter);
 
 export default router;
