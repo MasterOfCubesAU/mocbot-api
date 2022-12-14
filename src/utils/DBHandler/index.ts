36ecdefa59e4b6525ad3d158211534aede1b7479
MasterOfCubesAU <masterofcubesau@gmail.com>
2022-12-14 18:52:53 +1100

    Added getter methods. Closes #9

--- src/utils/DBHandler/index.ts
+++ src/utils/DBHandler/index.ts
@@ -1,17 +1,35 @@
-import mysql2 from 'mysql2';
+import mysql2, { OkPacket } from 'mysql2';
 
 class DatabaseHandler {
 
-    static connection = mysql2.createConnection({
+    static pool = mysql2.createPool({
         host: process.env.DB_HOST,
         user: process.env.DB_USER,
         password: process.env.DB_PASS,
-        database: process.env.DB_NAME
+        database: process.env.DB_NAME,
     });
 
+    static async field(query: string, values?: any | any[] | { [param: string]: any }) {
+        const [result, field] = (await DatabaseHandler.pool.promise().query(query, values))
+        return (Array.isArray(result) && result.length > 0) ? result[0][field[0].name] : null;
+    }
+    static async record(query: string, values?: any | any[] | { [param: string]: any }) {
+        const result = (await DatabaseHandler.pool.promise().query(query, values))[0]
+        return (Array.isArray(result) && result.length > 0) ? result[0] : {};
+    }
+    static async records(query: string, values?: any | any[] | { [param: string]: any }) {
+        return (await DatabaseHandler.pool.promise().query(query, values))[0]
+    }
+    static async column(query: string, values?: any | any[] | { [param: string]: any }) {
+        const [result, field] = (await DatabaseHandler.pool.promise().query(query, values))
+        return (Array.isArray(result)) ? result.map(row => row[field[0].name]) : []
+    }
+    static async execute(query: string, values?: any | any[] | { [param: string]: any }) {
+        await DatabaseHandler.pool.promise().execute(query, values);
+    }
 
-    static connect() {
-        DatabaseHandler.connection.connect((err) => {
+    static close() {
+        DatabaseHandler.pool.end((err) => {
             if (err) throw err;
         });
     }
