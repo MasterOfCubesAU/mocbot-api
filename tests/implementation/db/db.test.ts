859d5be3ef268b39db19d2ef84fa97eeb21705a8
MasterOfCubesAU <masterofcubesau@gmail.com>
2022-12-14 18:54:16 +1100

    Updated db test

--- tests/implementation/db/db.test.ts
+++ tests/implementation/db/db.test.ts
@@ -1,18 +1,2 @@
-import DB from "../../../src/utils/DBHandler";
-
-describe("Establish connection to DB", () => {
-    test('Valid connection', () => {
-
-        const VARS = [
-            process.env.DB_HOST,
-            process.env.DB_USER,
-            process.env.DB_PASS,
-            process.env.DB_NAME
-        ]
-
-        VARS.forEach(element => {
-            expect(element).not.toStrictEqual(undefined);
-        });
-        expect(() => { DB.connect() }).not.toThrow();
-    });
-});
+// TODO:
+// Need to use jest mock functions instead??
