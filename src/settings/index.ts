05c4f3829399146448b946ddd4ce374718156657
MasterOfCubesAU <masterofcubesau@gmail.com>
2022-12-19 04:36:22 +1100

    Added JSDoc

--- src/settings/index.ts
+++ src/settings/index.ts
@@ -2,6 +2,15 @@
 import DB from '@utils/DBHandler';
 import createErrors from 'http-errors';
 
+/**
+ * Stores settings for a given guild ID into the database
+ *
+ * @param guildID The guild ID to create settings for
+ * @param settings an object with key:value pairs of settings for this guild
+ * @throws {createErrors<400>}
+ * @throws {createErrors<409>}
+ * @returns {object} Empty object
+ */
 export async function createSettings(guildID: bigint | number, settings: object): Promise<any> {
   if (Object.keys(settings).length === 0) {
     throw createErrors(400, 'Settings for this guild must be provided.');
