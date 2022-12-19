8047f77664fbfc7662c8f5e608abb84b2e05af15
Samuel Zheng <z5418112@ad.unsw.edu.com>
2022-12-19 22:55:14 +1300

    fixed type checking

--- src/routes/v1/xp/index.ts
+++ src/routes/v1/xp/index.ts
@@ -1,17 +1,18 @@
 import express from 'express';
 import asyncHandler from 'express-async-handler';
 import { fetchGuildXP } from '@src/xp';
+import { Request, Response } from 'express';
 
 const router = express.Router();
 
 router.get(
   '/:guild_id',
-  asyncHandler(async (req, res) => {
+  asyncHandler(async (req: Request, res: Response) => {
     res.json(await fetchGuildXP(req.params.guild_id));
   })
 );
 
-router.get('/:guild_id/:user_id', (req, res) => {
+router.get('/:guild_id/:user_id', (req: Request, res: Response) => {
   return res.json({});
 });
 
