b5e0ad9495536a031547441946a9c69a344eb08b
MasterOfCubesAU <masterofcubesau@gmail.com>
2022-12-21 03:20:35 +1100

    Added types to server.ts

--- src/server.ts
+++ src/server.ts
@@ -6,6 +6,7 @@ import swaggerUi from 'swagger-ui-express';
 import YAML from 'yamljs';
 import asyncHandler from 'express-async-handler';
 import { validateSession } from '@src/auth';
+import { Request, Response, NextFunction } from 'express';
 
 // Import API routes
 import v1Route from '@routes/v1';
@@ -18,14 +19,14 @@ app.set('trust proxy', true);
 app.use(morgan(process.env.NODE_ENV !== 'production' ? 'dev' : 'common'));
 
 // Public Routes
-app.get('/', (req, res) => {
+app.get('/', (req: Request, res: Response) => {
   res.redirect('/docs');
 });
 
 app.use('/docs', swaggerUi.serve, swaggerUi.setup(YAML.load('docs/api.yml')));
 
 // All routes below this are authenticated
-app.use(asyncHandler(async (req, res, next) => {
+app.use(asyncHandler(async (req: Request, res: Response, next: NextFunction) => {
   await validateSession(req.headers['x-api-key'] as string);
   next();
 }));
