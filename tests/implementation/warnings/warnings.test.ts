1547ba12a759a458500fb0a24bbd9b105d664d38
MasterOfCubesAU <masterofcubesau@gmail.com>
2022-12-22 01:16:51 +1100

    Added testing for #22

--- tests/implementation/warnings/warnings.test.ts
+++ tests/implementation/warnings/warnings.test.ts
@@ -1,5 +1,5 @@
 import DB from '@utils/DBHandler';
-import { createWarning } from '@src/warnings';
+import { createWarning, getUserWarnings } from '@src/warnings';
 
 // Ensure DB is in a predictable state by clearing it initially, then again after every test
 // We then close the DB at the end to remove any open handles
@@ -23,5 +23,26 @@ describe('Create Warning', () => {
   test('Invalid (AdminID empty)', async () => {
     await expect(DB.execute('INSERT INTO UserInGuilds (UserID, GuildID) VALUES (?, ?)', [1, 1])).resolves.not.toThrow();
     await expect(createWarning(1, 1, 'Test Reason', null)).rejects.toThrow();
+  });
+});
+
+describe('Get Warnings', () => {
+  test('Valid (single value)', async () => {
+    await expect(DB.execute('INSERT INTO UserInGuilds (UserID, GuildID) VALUES (?, ?)', [1, 1])).resolves.not.toThrow();
+    await expect(createWarning(1, 1, 'Test Reason', 2)).resolves.not.toThrow();
+    await expect(getUserWarnings(1, 1)).resolves.not.toThrow();
+  });
+  test('Valid (multiple values)', async () => {
+    await expect(DB.execute('INSERT INTO UserInGuilds (UserID, GuildID) VALUES (?, ?)', [1, 1])).resolves.not.toThrow();
+    await expect(createWarning(1, 1, 'Test Reason', 2)).resolves.not.toThrow();
+    await expect(getUserWarnings(1, 1)).resolves.not.toThrow();
+  });
+  test('Invalid (UserID invalid)', async () => {
+    await expect(DB.execute('INSERT INTO UserInGuilds (UserID, GuildID) VALUES (?, ?)', [1, 1])).resolves.not.toThrow();
+    await expect(getUserWarnings(1, 2)).rejects.toThrow();
+  });
+  test('Invalid (GuildID invalid)', async () => {
+    await expect(DB.execute('INSERT INTO UserInGuilds (UserID, GuildID) VALUES (?, ?)', [1, 1])).resolves.not.toThrow();
+    await expect(getUserWarnings(2, 1)).rejects.toThrow();
   });
 });
