2cc9dd4939540d2db1f6588578aa8ed3800acc5c
MasterOfCubesAU <masterofcubesau@gmail.com>
2022-12-24 01:31:55 +1100

    Implemented #23

--- src/routes/v1/warnings/index.ts
+++ src/routes/v1/warnings/index.ts
@@ -1,13 +1,13 @@
 import express from 'express';
 import asyncHandler from 'express-async-handler';
-import { createWarning, deleteWarning, getUserWarnings } from '@src/warnings';
+import { createWarning, deleteWarning, getGuildWarnings, getUserWarnings } from '@src/warnings';
 import { Request, Response } from 'express';
 
 const router = express.Router();
 
-router.get('/:guild_id', (req, res) => {
-  return res.json({});
-});
+router.get('/:guild_id', asyncHandler(async (req: Request, res: Response) => {
+  res.json(await getGuildWarnings(BigInt(req.params.guild_id)));
+}));
 router.post('/:guild_id/:user_id', asyncHandler(async (req: Request, res: Response) => {
   res.json(await createWarning(BigInt(req.params.user_id), BigInt(req.params.guild_id), req.body.reason, req.body.adminID));
 }));
