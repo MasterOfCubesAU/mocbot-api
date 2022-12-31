8cb250ed7ba621d46ab1abf7c83f53418039b4e4
Sam Z <samuel.zheng1357@gmail.com>
2022-12-31 13:44:10 +1300

    added theme to api docs

--- src/server.ts
+++ src/server.ts
@@ -7,6 +7,7 @@ import YAML from 'yamljs';
 import asyncHandler from 'express-async-handler';
 import { validateSession } from '@src/auth';
 import { Request, Response, NextFunction } from 'express';
+import { SwaggerTheme } from 'swagger-themes';
 
 // Import API routes
 import v1Route from '@routes/v1';
@@ -15,16 +16,24 @@ import v1Route from '@routes/v1';
 const app = express();
 app.use(express.json());
 app.use(cors());
+app.use('/public', express.static('public'));
 app.set('trust proxy', true);
 app.set('json replacer', (_: any, value: any) => (typeof value === 'bigint' ? value.toString() + 'n' : value));
 app.use(morgan(process.env.NODE_ENV !== 'production' ? 'dev' : 'common'));
+const theme = new SwaggerTheme('v3');
+
+const swaggerOptions = {
+  customCss: theme.getBuffer('dark'),
+  customSiteTitle: 'MOCBOT API',
+  customfavIcon: '/public/favicon.ico',
+};
 
 // Public Routes
 app.get('/', (req: Request, res: Response) => {
   res.redirect('/docs');
 });
 
-app.use('/docs', swaggerUi.serve, swaggerUi.setup(YAML.load('docs/api.yml')));
+app.use('/docs', swaggerUi.serve, swaggerUi.setup(YAML.load('docs/api.yml'), swaggerOptions));
 
 // All routes below this are authenticated
 app.use(
